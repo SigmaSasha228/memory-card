17b87979d6c886059510a6fc74df6c35352b8f8b
Jamkal <121348893+SigmaSasha228@users.noreply.github.com>
2022-12-24 17:52:39 +0300

    Update memory_card.py
    version 1.2

--- memory_card.py
+++ memory_card.py
@@ -95,3 +95,53 @@ while run:
         if num_fire >= 50 and rel_time == False:
             start = timer()
             rel_time = True
+           
+    if not finish:
+        window.blit(background, (0, 0))
+        ship.reset()
+        ship.update()
+        monsters.draw(window)
+        monsters.update()
+        bullets.draw(window)
+        bullets.update()
+        asteroids.draw(window)
+        asteroids.update()
+        text_lost = font1.render('Пропущено:' + str(lost), True, (255, 255, 255))
+        text_kills = font1.render('Сбитых:' + str(kills), True, (255, 255, 255))
+        text_life = font1.render('Жизни:' + str(life), True, (255, 255, 255))
+        window.blit(text_lost, (10, 60))
+        window.blit(text_kills, (10, 20))
+        window.blit(text_life, (10, 100))
+        if rel_time == True:
+            new = timer()
+            if new - start < 3:
+                reload = font1.render('Перезарядка....', True, (255, 255, 255))
+                window.blit(reload, (400, 350))
+            else:
+                rel_time = False
+                num_fire = 0
+        collides = sprite.groupcollide(monsters, bullets, True, True)
+        for c in collides:
+            kills += 1
+            monster = Enemy('zombi-removebg-preview.png', randint(40, 660), -40, 80, 40, randint(2,5))
+            monsters.add(monster)
+        if sprite.spritecollide(ship, monsters, False)  or sprite.spritecollide(ship, asteroids, False):
+            life -= 1
+            sprite.spritecollide(ship, monsters, True)
+            sprite.spritecollide(ship, asteroids, True)
+        if life == 0 or lost >= 10:
+            finish = True
+            window.blit(lose, (400, 350))
+        if kills >= 10:
+            finish = True
+            window.blit(win, (400, 350))
+    else:
+        finish = False
+        kills = 0
+        lost = 0
+        time.delay(3000)
+
+        for m in monsters:
+            m.kill()
+        for b in bullets:
+            b.kill()
